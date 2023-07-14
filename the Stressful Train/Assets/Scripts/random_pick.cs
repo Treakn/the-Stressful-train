be9877cda14d3b27aab4e550f2274b34afba75e1
Treakn <103149140+Treakn@users.noreply.github.com>
2023-07-14 17:28:35 +0300

    60%

---
+++ the Stressful Train/Assets/Scripts/random_pick.cs
@@ -0,0 +1,46 @@
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class NPCRequestFunction : MonoBehaviour
+{
+    public enum RequestType
+    {
+        GetWater,
+        GetNapkins,
+        GetFood
+    }
+
+    private RequestType currentRequest;
+    private float requestTime;
+    private float timeIntervalMin;
+    private float timeIntervalMax;
+
+    void Start()
+    {
+        currentRequest = RequestType.GetWater;
+        requestTime = 0.0f;
+        //time intervals 
+        timeIntervalMin = 10.0f;     // 1 minute
+        timeIntervalMax = 90.0f;     // 1.5 minutes
+    }
+
+    void Update()
+    {
+        requestTime += Time.deltaTime;
+        //logic to reset time after choosing
+        if (requestTime >= timeIntervalMin && requestTime <= timeIntervalMax)
+        {
+            ChooseRandomRequest();
+            requestTime = 0.0f;
+        }
+    }
+    //choosing the random request
+    private void ChooseRandomRequest()
+    {
+        int requestIndex = Random.Range(0, 3);
+        currentRequest = (RequestType)requestIndex;
+
+        Debug.Log("The NPC requests " + currentRequest);
+    }
+}
